fafbc78a0e95344c2c0b7bfafd7510c64a22443e
jbelle <jordie.r.belle@gmail.com>
2023-05-02 02:34:25 +0000

    add documentation and typing to bot.py

--- src/seria/bot.py
+++ src/seria/bot.py
@@ -2,12 +2,10 @@ import os
 import json
 import traceback
 from dotenv import load_dotenv
-
 import discord
 from discord import Intents, ApplicationContext, Interaction, Embed
 from discord.ext import commands
 from discord.ui import Modal, View, InputText, button
-
 from utils.lc_handler import LangChainHandler
 from utils.logger import CustomLogger
 
@@ -34,31 +32,82 @@ with open(data_file, 'r') as f:
 posts = [post for post in data['blog'].values()]
 
 class ThumbsDownFeedbackModal(Modal):
+    """
+    A modal that is used to gather feedback from users who have marked the answer as "thumbs down."
+    """
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.add_item(InputText(label='Feedback? Resource links welcome!', style=discord.InputTextStyle.long))
 
     async def callback(self, interaction: Interaction):
+        """
+        The callback function for the ThumbsDownFeedbackModal.
+
+        When a user submits feedback, this function sends a message to the user to let them know
+        their feedback has been received.
+
+        Args:
+            interaction (Interaction): The user interaction that triggered the modal.
+        """
         embed = Embed(title="Thank you for your feedback!")
         embed.add_field(name="We'll take a look at the following...", value=self.children[0].value)
         await interaction.response.send_message(embeds=[embed])
 
 class FeedbackView(View):
+    """
+    A view that is shown to the user after a query has been answered.
+
+    This view allows the user to provide feedback on the answer they received.
+    """
     @button(style=discord.ButtonStyle.green, emoji="👍")
-    async def up_button_callback(self, button, interaction: Interaction):
+    async def up_button_callback(self, button: button, interaction: Interaction):
+        """
+        The callback function for the "thumbs up" button.
+
+        When a user clicks the "thumbs up" button, this function sends a message to the user to let them
+        know their feedback has been received.
+
+        Args:
+            button: The button object.
+            interaction (Interaction): The user interaction that triggered the view.
+        """
         await interaction.response.send_message("Thank you for the feedback!", ephemeral=True)
         
     @button(style=discord.ButtonStyle.red, emoji="👎")
-    async def down_button_callback(self, button, interaction: Interaction):
+    async def down_button_callback(self, button: button, interaction: Interaction):
+        """
+        The callback function for the "thumbs down" button.
+
+        When a user clicks the "thumbs down" button, this function displays the ThumbsDownFeedbackModal.
+
+        Args:
+            button: The button object.
+            interaction (Interaction): The user interaction that triggered the view.
+        """
         await interaction.response.send_modal(ThumbsDownFeedbackModal(title='ThumbsDownFeedbackModal'))
 
 @bot.event
 async def on_ready():
+    """
+    An event that is triggered when the bot is ready.
+
+    This function logs that the bot is ready and online.
+    """
     logger.info(f"{bot.user} is ready and online!")
 
 # Ask command
 @bot.slash_command(description='Ask Seria a DFO-related question')
 async def ask(ctx: ApplicationContext, *, question: str):
+    """
+    The function that handles the "ask" command.
+
+    When a user enters a query, this function processes the query and returns an answer. It also shows the user a
+    FeedbackView, which allows them to provide feedback on the answer they received.
+
+    Args:
+        ctx (ApplicationContext): The context of the command.
+        question (str): The query that the user entered.
+    """
     global posts
     
     # Show user bot is thinking
@@ -72,8 +121,7 @@ async def ask(ctx: ApplicationContext, *, question: str):
 
     # Send the answer back to the user
     follow_up_text = f'Q: {question}\n\nA: {answer}\n\nPlease give this answer feedback with the buttons below!'
-    await ctx.send_followup(follow_up_text, ephemeral=False, view=FeedbackView())
-    # await ctx.respond("Was the response helpful?", )
+    await ctx.send_followup(follow_up_text, ephemeral=False,
 
 # Ask command error handler
 @ask.error
